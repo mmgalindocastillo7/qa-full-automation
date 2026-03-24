9624ff2a3b91a6e1da5652cca9c159eb9b020df7
Marriamer <mgalindocastillo6@gmail.com>
2026-03-24 05:17:36 -0500

    fix: implement loginpage and stabilize test (login, cart, add product, api)

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,13 +1,11 @@
 import { test, expect } from '@playwright/test';
-
+import { LoginPage } from '../pages/LoginPage'
 test('Login test', async ({ page }) => {
 
-  await page.goto('https://www.saucedemo.com/');
+  const loginPage = new LoginPage(page);
 
-  await page.fill('#user-name', 'standard_user');
-  await page.fill('#password', 'secret_sauce');
-
-  await page.click('#login-button');
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
 
   await expect(page).toHaveURL(/inventory/);
 
