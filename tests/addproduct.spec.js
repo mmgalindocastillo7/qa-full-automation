3374b5970e77b7eb9265ac25a571c99957ac1731
Marriamer <mgalindocastillo6@gmail.com>
2026-03-25 12:50:24 -0500

    feat; add playwright report, screenshots and stable tests

--- tests/addproduct.spec.js
+++ tests/addproduct.spec.js
@@ -2,24 +2,21 @@ import { test, expect } from '@playwright/test';
 
 test('Add product to cart', async ({ page }) => {
 
+  // 👇 1. Ir a la página
   await page.goto('https://www.saucedemo.com/');
 
+  // 👇 2. Login
   await page.fill('#user-name', 'standard_user');
   await page.fill('#password', 'secret_sauce');
-
   await page.click('#login-button');
 
+  // 👇 3. ESPERAR que cargue bien (CLAVE 🔥)
   await page.waitForURL('**/inventory.html');
 
-  const addButton = page.locator('#add-to-cart-sauce-labs-backpack');
-
-  await expect(addButton).toBeVisible();
-
-  await addButton.click();
+  // 👇 4. Ahora sí interactuar
+  await page.click('#add-to-cart-sauce-labs-backpack');
 
   await page.click('.shopping_cart_link');
 
-  await expect(page.locator('.inventory_item_name'))
-    .toContainText('Sauce Labs Backpack');
-
+  await expect(page).toHaveURL(/cart.html/);
 });
